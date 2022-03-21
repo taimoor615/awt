6badc3abb54982602131a25cc93880d6c70cac15
taimoor615 <taimoor615>
2022-03-21 15:58:48 +0500

    Conatc File and Gallery File Added

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,13 @@ jQuery(document).ready(function (){
         jQuery(".menu").slideToggle();
     });
 });
+
+/*========================
+        Gallery
+ ========================*/
+$('a.gallery').featherlightGallery({
+    previousIcon: '',     /* Code that is used as previous icon */
+    nextIcon: '',         /* Code that is used as next icon */
+    galleryFadeIn: 100,          /* fadeIn speed when slide is loaded */
+    galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
+});
